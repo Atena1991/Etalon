f0adcec2ed23d6a1ae78a707da93553e89c72219
Atena1991 <atenagorjii@gmail.com>
2023-01-17 19:00:52 +0330

    first commit

---
+++ etalon2/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+    <body>
+        <Header/>
+        <Navbar/>
+        <main>
+            <BrandCooperation/>
+            <NewCollection/>
+            <BestSeller/>
+        </main>
+        <Footer/>
+    </body>
+</template>
+<script>
+    import Header from './components/Header.vue'
+    import Navbar from './components/Navbar.vue'
+    import BrandCooperation from './components/BrandCooperation.vue'
+    import NewCollection from './components/NewCollection.vue'
+    import BestSeller from './components/BestSeller.vue'
+    import Footer from './components/Footer.vue'
+
+    export default {
+        name:"App",
+        components:{
+            Header,
+            Navbar,
+            BrandCooperation,
+            NewCollection,
+            BestSeller,
+            Footer
+        }
+    }
+</script>
+<!-- <style scoped>
+.btn {
+  font-size: var(--fs-btn);
+  border-radius: 6px;
+}
+.btn-yellow {
+  background: var(--secondary-background-color);
+  color: var(--fc-dark);
+}
+.btn-yellow:hover {
+  background: #FFEB9D;
+}
+.btn-black,
+.btn-black2 {
+  background: #181818;  
+  color: var(--fc-light);
+  padding: 16px 40px;
+  line-height: 17px;
+}
+.btn-black:hover {
+  background: #1E1E1E;
+  color: #FFFFFF;
+}
+.btn-black2:hover {
+  background: #1E1E1E;
+  color: #FFFFFF;
+} 
+.shoes{
+  z-index:3 ;
+}
+</style> -->
+
+
+
---
+++ etalon2/src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+<nav class="img-navbar d-lg-none d-xl-none d-xxl-none ">
+    <div class="container">
+        <ul class="d-flex justify-content-between align-content-center list-unstyled">
+            <li>
+                <a href="#"><img src="src/assets/images/phone.svg" alt="phone"></a>
+            </li>
+            <li>
+                <a href="#"><img src="src/assets/images/tag.svg" alt="tag"></a>
+            </li>
+            <li>
+                <a href="#"><img src="src/assets/images/home.svg" alt="home"></a>
+            </li>
+            <li>
+                <a href="#"><img src="src/assets/images/store.svg" alt="store"></a>
+            </li>
+            <li>
+              <span>{{ counterStore.count }}</span>
+              <img src="src/assets/images/cart1.svg" alt="cart">
+            </li>
+        </ul>
+    </div>
+</nav>
+</template>
+<script>
+import { useCounterStore } from '../stores/counter';
+    import { mapStores } from 'pinia';
+    export default{
+        computed:{
+            ...mapStores(useCounterStore)
+        }
+    }
+</script>
+<!-- <style scoped>
+.navbar {
+  font-family: var(--body-font-family);
+  margin-inline: 16px;
+}
+.navbar a:hover {
+  color: var(--fc-light);
+}
+
+#cart:hover {
+  transform-origin: conter;
+  transform: scale(1.2);
+  cursor: pointer;
+}
+.cart:hover {
+  transform-origin: conter;
+  transform: scale(1.1);
+  cursor: pointer;
+  opacity: 0.9;
+}
+</style> -->
